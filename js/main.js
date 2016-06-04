43abf374519e36a634e69660ad64239910665be1
Peter Krieg <pkrieg2@gmail.com>
2016-06-04 02:43:20 -0600

    trying to work with email form

--- js/main.js
+++ js/main.js
@@ -11,11 +11,27 @@ $(document).ready(function() {
 
   $('button.get-in-touch').click(function(){
     console.log('clicked!!');
+    toggleEmailForm(true);
 
   });
 
+  $(window).keydown(function(evt){
+    var keycode = evt.which;
+    //  27 is escape, 13 is enter 
+    if($('body').hasClass('show-email-form') && (keycode === 27 || keycode === 13)) {
+      toggleEmailForm(false);
+    }
+  });
+
+  $('div.close-form').click(toggleEmailForm.bind(null, false));
+
   // state true is to show it, false is to hide it
   function toggleEmailForm(state){
+    var $body = $('body');
+    $('.email-form').toggleClass('hidden', !state);
+    $body.toggleClass('show-email-form', state);
+
+
 
 
 
